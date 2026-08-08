f62b54b8f09fc94dbfc7ce42039a292f63dfcdcc
Delsff <daniilpodlesovskiy@gmail.com>
2026-08-08 20:59:36 +0200

    fix

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -3,9 +3,10 @@ import axios from 'axios';
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
   headers: {
-    'Content-type': 'application/json',
+    'Content-Type': 'application/json',
   },
 });
+
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
   if (token) {
@@ -13,4 +14,5 @@ api.interceptors.request.use((config) => {
   }
   return config;
 });
+
 export default api;
